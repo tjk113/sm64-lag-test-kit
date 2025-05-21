65996621866c10adef3e98ea8c555718a5f20418
tjk113 <tjkuhl1@live.com>
2025-05-21 05:43:15 -0400

    Add a few frames of delay before loading TAS data
    Before the logo appears, the game engine apparently isn't fully
    functional. This caused a crash when trying to write TAS data
    to RAM. This commit delays loading the TAS data for 5 frames,
    thereby avoiding this console crash.

--- src/custom/custom.c
+++ src/custom/custom.c
@@ -3,11 +3,17 @@
 #include "lag.h"
 #include "tas_playback.h"
 
+u32 start_delay_frames = 5;
+
 void custom_entry(void *func, s32 eventId) {
     if (func == profiler_log_thread5_time) {
         if (eventId == THREAD5_END) {
             curFrameViCount = 0;
         } else if (eventId == INPUT_POLL) {
+            if (start_delay_frames > 0) {
+                start_delay_frames -= 1;
+                return;
+            }
             update_playback();
         }
     } else if (func == profiler_log_vblank_time) {
